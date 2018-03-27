ea6c5316bd93c8c1c0475c614326bbb00fb0fe7e
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-27 15:29:56 +0200

    Fix script

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const list = document.getElementById('list');
 const add = document.getElementById('addElem');
 add.addEventListener('click', function() {
     const element = document.createElement('li');
-    const num = document.getElementById('list').getElementsByTagName('li');
+    const num = list.getElementsByTagName('li');
     element.innerHTML = ('item ' + num.length);
     list.appendChild(element);
 });
